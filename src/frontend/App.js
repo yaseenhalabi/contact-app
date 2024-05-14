d5cb341b5c17b0fb09e998cbd545fb6d020fec0f
Yaseen Halabi <yaseenhalabi@gmail.com>
2024-05-14 10:43:30 -0500

    added navigation

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -2,15 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
 import HomeScreen from './screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
 export default function App() {
+
   const [fontsLoaded] = useFonts({
     'Trebuc': require('./assets/fonts/trebuc.ttf'),
   });
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <HomeScreen />
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
       <StatusBar style="light" />
-    </View>
+    </NavigationContainer>
   );
 }
 
