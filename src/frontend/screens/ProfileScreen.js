ab936e0c649518db8159acd99af7af00d47c4129
Yaseen Halabi <yaseenhalabi@gmail.com>
2024-05-21 11:06:49 -0500

    feat: Update dependencies for react-native-gesture-handler and react-native-swipe-gestures

--- src/frontend/screens/ProfileScreen.js
+++ src/frontend/screens/ProfileScreen.js
@@ -1,48 +1,66 @@
 import { View, SafeAreaView, Text, TouchableOpacity, StyleSheet, ScrollView, Image } from 'react-native';
 import ryanMatia from '../assets/images/ryanmatia.jpg';
 import billWalsh from '../assets/images/billwalsh.jpg';
-
+import instagramLogo from '../assets/icons/instagramlogowhite.png';
+import xLogo from '../assets/icons/xlogowhite.png';
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
 import { COLORS } from '../utils/colors';
-
+import backArrowIcon from '../assets/icons/backarrowicon.png';
 export default function ProfileScreen({ navigation }) {
+    const onSwipeRight = (gestureState) => {
+        navigation.pop();
+    }
     return (
-        <View style={styles.container}>
-            <View style={styles.section}>
-                <SafeAreaView>
-                    <Text style={styles.titleText}>John Smith</Text>
-                </SafeAreaView>
-                <View style={styles.tagsContainer}>
-                    <View style={styles.tag}>
-                        <Text style={styles.tagText}>Water Polo</Text>
+        <GestureRecognizer
+            onSwipeRight={(state) => onSwipeRight(state)}
+            style={styles.container}
+        >
+            <View style={styles.container}>
+                <View style={styles.section}>
+                    <SafeAreaView>
+                        <Text style={styles.titleText}>John Smith</Text>
+                    </SafeAreaView>
+                    <View style={styles.tagsContainer}>
+                        <View style={styles.tag}>
+                            <Text style={styles.tagText}>Water Polo</Text>
+                        </View>
+                    </View>
+                    <View style={styles.birthdayContainer}>
+                        <Text style={[styles.birthdayText, styles.boldBirthday]}>Birthday: </Text>
+                        <Text style={styles.birthdayText}>2/12/2006</Text>
+                        <Text style={styles.birthdayTimingText}> (in 334 days)</Text>
                     </View>
                 </View>
-                <View style={styles.birthdayContainer}>
-                    <Text style={[styles.birthdayText, styles.boldBirthday]}>Birthday: </Text>
-                    <Text style={styles.birthdayText}>2/12/2006</Text>
-                    <Text style={styles.birthdayTimingText}> (in 334 days)</Text>
+                <View style={[styles.section, {backgroundColor: COLORS.secondary, height: 300}]}>
+                    <ScrollView>
+                        <Text style={styles.subTitle}>Notes</Text>
+                        <Text style={styles.birthdayText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, vestibulum mi nec, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.</Text>
+                        <Text style={styles.birthdayText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, vestibulum mi nec, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.</Text>
+                        <Text style={styles.birthdayText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, vestibulum mi nec, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.</Text>
+                    </ScrollView>
                 </View>
-            </View>
-            <View style={[styles.section, {backgroundColor: COLORS.secondary, height: 200}]}>
-                <ScrollView>
-                    <Text style={styles.subTitle}>Notes</Text>
-                    <Text style={styles.birthdayText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, vestibulum mi nec, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.</Text>
-                    <Text style={styles.birthdayText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, vestibulum mi nec, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.</Text>
-                    <Text style={styles.birthdayText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, vestibulum mi nec, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.</Text>
-                </ScrollView>
-            </View>
-            <View style={styles.section}>
-                <Text style={[styles.subTitle, {marginTop: 20}]}>Photos</Text>
-                <View style={styles.images}>
-                    <Image style={styles.image} source={ryanMatia}/>
-                    <Image style={styles.image} source={billWalsh}/>
+                <View style={styles.section}>
+                    <Text style={[styles.subTitle, {marginTop: 20}]}>Photos</Text>
+                    <View style={styles.images}>
+                        <Image style={styles.image} source={ryanMatia}/>
+                        <Image style={styles.image} source={billWalsh}/>
 
+                    </View>
                 </View>
+                <TouchableOpacity onPress={() => navigation.pop()} style={styles.backArrowIconContainer}>
+                    <Image source={backArrowIcon} style={{width: 30, height: 30}}/>
+                </TouchableOpacity>
+                <View style={styles.socialsContainer}>
+                    <TouchableOpacity>
+                        <Image source={instagramLogo} style={{width: 40, height: 40}}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image source={xLogo} style={{width: 40, height: 40}}/>
+                    </TouchableOpacity>
+                </View>
+
             </View>
-            <TouchableOpacity onPress={() => navigation.pop()}>
-                <Text>Go Back A Page</Text>
-            </TouchableOpacity>
-            <View style={styles.socialsContainer}></View>
-        </View>
+        </GestureRecognizer>
     )  
 }
 
@@ -116,8 +134,15 @@ styles = StyleSheet.create({
         gap: 10,
     },
     socialsContainer: {
-        padding: 20,
         flexDirection: 'row',
-        justifyContent: 'space-around',
+        gap: 20,
+        position: 'absolute',
+        bottom: 30,
+        left: 20,
     },
+    backArrowIconContainer: {
+        position: 'absolute',
+        top: 22,
+        right: 20,
+    }
 }); 
