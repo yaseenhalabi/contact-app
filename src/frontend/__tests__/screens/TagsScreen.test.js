15b70742ad27a4b35d3c9501dd12b053f8d7871a
Yaseen Halabi <yaseenhalabi@gmail.com>
2024-06-17 17:46:44 -0500

    wrote simple render test for tagsscreen

--- src/frontend/__tests__/screens/TagsScreen.test.js
+++ src/frontend/__tests__/screens/TagsScreen.test.js
@@ -1,50 +1,15 @@
 import React from 'react';
-import TagScreen from '../../screens/TagsScreen';
-import { render, screen, fireEvent } from '@testing-library/react-native';
+import { render } from '@testing-library/react-native';
 import { Provider } from 'react-redux';
-import store from '../../redux/store';
+import TagsScreen from '../../screens/TagsScreen';
+import store from '../../redux/store'; // Adjust the import path as needed
 
-// Test if the search input is rendered
-test('renders search input', () => {
+describe('TagsScreen', () => {
+  test('renders TagsScreen component', async () => {
     render(
-        <Provider store={store}>
-            <TagScreen />
-        </Provider>
+      <Provider store={store}>
+        <TagsScreen />
+      </Provider>
     );
-    const searchInput = screen.getByPlaceholderText('Search...');
-    expect(searchInput).toBeTruthy();
-});
-
-// Test if the "+ New Tag" button is rendered
-test('renders "+ New Tag" button', () => {
-    render(
-        <Provider store={store}>
-            <TagScreen />
-        </Provider>
-    );
-    const newTagButton = screen.getByText('+ New Tag');
-    expect(newTagButton).toBeTruthy();
-});
-// Test if the tag input is rendered when "+ New Tag" button is pressed
-test('renders tag input when "+ New Tag" button is pressed', () => {
-    render(
-        <Provider store={store}>
-            <TagScreen />
-        </Provider>
-    );
-    const newTagButton = screen.getByText('+ New Tag');
-    fireEvent.press(newTagButton);
-    const tagInput = screen.getByPlaceholderText('Enter tag...');
-    expect(tagInput).toBeTruthy();
-});
-
-// Test if the tag input is not rendered when "+ New Tag" button is not pressed
-test('does not render tag input when "+ New Tag" button is not pressed', () => {
-    render(
-        <Provider store={store}>
-            <TagScreen />
-        </Provider>
-    );
-    const tagInput = screen.queryByPlaceholderText('Enter tag...');
-    expect(tagInput).toBeNull();
+  });
 });
