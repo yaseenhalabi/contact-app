c8ad16d53a105a8ca25961c79082d1a3b92b08f5
Yaseen Halabi <yaseenhalabi@gmail.com>
2024-06-17 17:14:10 -0500

    fixed act() warning for app.test.js

--- src/frontend/__tests__/App.test.js
+++ src/frontend/__tests__/App.test.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import { render } from '@testing-library/react-native';
 import App from '../App';
 
-import { render } from '@testing-library/react-native';
-test('renders', () => {
-  render(<App />);
+jest.mock('expo-font', () => ({
+  useFonts: () => [true, false],
+}));
+
+jest.mock('../redux/store', () => ({
+  getState: jest.fn(),
+  subscribe: jest.fn(),
+  dispatch: jest.fn(),
+}));
+
+jest.mock('@react-navigation/material-top-tabs', () => {
+  return {
+    createMaterialTopTabNavigator: jest.fn().mockReturnValue({
+      Navigator: ({ children }) => <>{children}</>,
+      Screen: ({ children }) => <>{children}</>,
+    }),
+  };
 });
 
+jest.mock('@react-navigation/native', () => {
+  return {
+    NavigationContainer: ({ children }) => <>{children}</>,
+  };
+});
 
+jest.mock('@react-navigation/native-stack', () => {
+  return {
+    createNativeStackNavigator: jest.fn().mockReturnValue({
+      Navigator: ({ children }) => <>{children}</>,
+      Screen: ({ children }) => <>{children}</>,
+    }),
+  };
+});
+
+jest.mock('react-native-gesture-handler', () => {
+  return {
+    GestureHandlerRootView: ({ children }) => <>{children}</>,
+  };
+});
+
+test('renders App component', () => {
+  const { getByTestId } = render(<App />);
+  // expect(getByTestId('app-component')).toBeTruthy();
+  // expect(getByTestId('tab-bar')).toBeTruthy();
+  // expect(getByTestId('people-screen')).toBeTruthy();
+  // expect(getByTestId('tags-screen')).toBeTruthy();
+  // expect(getByTestId('status-bar')).toBeTruthy();
+});
