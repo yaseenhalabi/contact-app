968ac395876cad91a6113df4dcf3f4b858e706cf
Yaseen Halabi <yaseenhalabi@gmail.com>
2024-07-26 13:29:55 -0500

    other: added some fake data

--- src/frontend/redux/tagsSlice.js
+++ src/frontend/redux/tagsSlice.js
@@ -3,13 +3,13 @@ import { createSlice } from '@reduxjs/toolkit';
 export const tagsSlice = createSlice({
     name: 'tags',
     initialState: [
-        {id: '1', name: 'Hinsdale Central', color: '#df4040'}, 
-        {id: '2', name: 'Asbahi Family', color: '#c4438a'}, 
-        {id: '3', name: 'People I Hate', color: '#54a145'}, 
+        {id: '1', name: 'Work', color: '#df4040'}, 
+        {id: '2', name: 'Grant Family', color: '#c4438a'}, 
+        {id: '3', name: 'Poetry Club', color: '#54a145'}, 
         {id: '4', name: 'Computer Science Club', color: '#1794a0'},
         {id: '5', name: 'University of California', color: '#e050bf'}, 
-        {id: '6', name: 'Smith Family', color: '#2baa6a'}, 
-        {id: '7', name: 'Harvard University', color: '#3446cc'}, 
+        {id: '6', name: 'Rowing Team', color: '#2baa6a'}, 
+        {id: '7', name: 'My Age', color: '#3446cc'}, 
         {id: '8', name: 'Johnson Family', color: '#c2114c'}
     ],
     reducers: {
